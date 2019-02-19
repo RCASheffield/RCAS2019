9d97f82c3cbaf46616dbf188d4ee3900dddc90e3
mbjackson1 <39310293+mbjackson1@users.noreply.github.com>
2019-02-19 19:57:28 +0000

    Define Variables

---
+++ LocoArduino/LocoArduino.ino
@@ -0,0 +1,27 @@
+
+// Output Variables
+boolean brakes; // BRAKES relay
+boolean power;  // POWER ISOLATE relay
+boolean cap;   // CAP ISOLATE relay
+boolean rheo; // RHEO relay
+boolean sd;  // Driver shutdown pins
+int pwm;   // Output to MOSFET drivers
+
+// Recieved Variables
+boolean deadman;
+boolean horn; // Horn drive transistor (recieved directly)
+char drive;
+boolean regen;
+char mode;
+boolean auto_stop;
+int desired_speed;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
