5e6abb6a8dfc8d54bbb589017d505d583a3c6ac2
MJ <m_b_jackson@yahoo.co.uk>
2019-03-26 19:54:41 +0000

    Update LocoArduino.ino
    State heirachy and received communications setup

--- LocoArduino/LocoArduino.ino
+++ LocoArduino/LocoArduino.ino
@@ -1,3 +1,5 @@
+// Output pin definitions
+const int horn_pin = 4;
 
 // Output Variables
 boolean brakes; // BRAKES relay
@@ -9,7 +11,6 @@ int pwm;   // Output to MOSFET drivers
 
 // Recieved Variables
 boolean deadman;
-boolean horn; // Horn drive transistor (recieved directly)
 char drive;
 boolean regen;
 char mode;
@@ -17,37 +18,122 @@ boolean auto_stop;
 int desired_speed;
 
 int state;
+boolean exit_flag;
 
-void state_activate (int state){
-  if (state == 1 ){
-    brakes = 0;
-    power = 1;
-    cap = 1;
-    rheo = 1;
-  }
-  if (state == 2 ){
-    brakes = 1;
-    power = 1;
-    cap = 1;
-    rheo = 1;
-  }
-}
+const int timeout = 100;
+unsigned long deadman_time;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  common_function();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- if (deadman == 0){
-  //Neutral & Parking brake on (STATE 1)
-  state_activate (1);
- }
- else {
-  if (drive == N) {
-    //Neutral (Shutdown low) (STATE 2)
-    state_activate (2);
+  exit_flag=0;
+  while(exit_flag==0&&drive=='N'){
+  // Neutral, shutdown pin low
+  common_function();
+  
   }
-  else (
- }
+  while(exit_flag==0&&drive!='N'){
+    while(exit_flag==0&&regen==0){
+      while(exit_flag==0&&auto_stop==0){
+      // Standard, forward/backward drive, speed set by controller
+      common_function();
+      
+      }
+      while(exit_flag==0&&auto_stop==1){
+      // Standard, forward/backward drive, speed set by controller, but enters auto stop when signal recieved
+      common_function();
+      
+      }
+    }
+    while(exit_flag==0&&regen==1){
+      while(exit_flag==0&&mode=='D'){
+      // Forward/backward drive, speed set by controller, but capacitor disconnected and emptied
+      common_function();
+      
+      }
+      while(exit_flag==0&&mode=='C'){
+      // Automatic regen collection, battery disconnected, slows train down to stop within required distance
+      common_function();
+      
+      }
+      while(exit_flag==0&&mode=='U'){
+      // Forward/backward drive, speed set by controller, batteries disconnected, power from capacitor
+      common_function();
+      
+      }
+    }
+  }
+}
+
+void common_function(){
+  communications();
+  while((millis()-deadman_time) > timeout){ // deadman timeout
+    // Neutral, parking brake on
+    communications(); 
+  }
+}
+
+void communications(){
+  // Received data
+  while (Serial.available()>0){
+    incoming_byte = Serial.read();
+    if (incoming_byte=='X'){
+      deadman = 1;
+      deadman_time = millis();
+    }
+    if(incoming_byte=='h'){
+      digitalWrite(horn_pin,0);
+    }
+    if(incoming_byte=='H'){
+      digitalWrite(horn_pin,1);
+    }
+    if(incoming_byte=='N'){
+      drive = 'N';
+      exit_flag = 1;
+    }
+    if(incoming_byte=='F'){
+      drive = 'F';
+      exit_flag = 1;
+    }
+    if(incoming_byte=='B'){
+      drive = 'B';
+      exit_flag = 1;
+    }
+    if(incoming_byte=='r'){
+      regen = 0;
+      exit_flag = 1;
+    }
+    if(incoming_byte=='R'){
+      regen = 1;
+      exit_flag = 1;
+    }
+    if(incoming_byte=='D'){
+      mode = 'D';
+      exit_flag = 1;
+    }
+    if(incoming_byte=='C'){
+      mode = 'C';
+      exit_flag = 1;
+    }
+    if(incoming_byte=='U'){
+      mode = 'U';
+      exit_flag = 1;
+    }
+    if(incoming_byte=='a'){
+      auto_stop = 0;
+      exit_flag = 1;
+    }
+    if(incoming_byte=='A'){
+      auto_stop = 1;
+      exit_flag = 1;
+    }
+    if(incoming_byte=='s'){
+      desired_speed = Serial.parseInt();
+    }
+    // Send data
+    
 }
